104a098cfe3f42ca72d1c15f11018a62b265a7c7
kaue schultz <kaue.schultz@paygo.com.br>
2019-04-30 13:44:27 -0300

    initial commit

---
+++ src/test/kotlin/com/kschltz/kafkastreams/KafkaStreamsApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.kschltz.kafkastreams
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KafkaStreamsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
